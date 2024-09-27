eb4937a2715dfec43990744bd6bd17048f42770c
Biraj Pokharel <biraj.pokharel411@gmail.com>
2024-09-27 00:54:05 +0000

    creating db model for particular order customers total amount spend on each order

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,11 @@
-select 
+select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
 
+    -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
-    created as created_as
-from raw.stripe.payment    
+    created as created_at
+
+from raw.stripe.payment 
